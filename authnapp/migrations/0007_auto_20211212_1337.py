986058ede80855b35317a53228185d7a9a606f5a
Denis Dudnik <Denis.Dudnik@gmail.com>
2021-12-18 11:17:43 +0300

    Lesson 16. Code style fix

--- authnapp/migrations/0007_auto_20211212_1337.py
+++ authnapp/migrations/0007_auto_20211212_1337.py
@@ -9,13 +9,16 @@ from django.utils.timezone import utc
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('authnapp', '0006_auto_20211208_1817'),
+        ("authnapp", "0006_auto_20211208_1817"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='shopuser',
-            name='activation_key_expires',
-            field=models.DateTimeField(default=datetime.datetime(2021, 12, 14, 13, 37, 26, 765066, tzinfo=utc), verbose_name='актуальность ключа'),
+            model_name="shopuser",
+            name="activation_key_expires",
+            field=models.DateTimeField(
+                default=datetime.datetime(2021, 12, 14, 13, 37, 26, 765066, tzinfo=utc),
+                verbose_name="актуальность ключа",
+            ),
         ),
     ]
